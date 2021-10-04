13e370e2d6f1410508766274bdbffda3a3bdbc60
egprivalov <44251572+egprivalov@users.noreply.github.com>
2021-10-04 12:46:37 +0500

    Update taxi.py
    Программа разработана для решения задачи по отправке n сотрудников на n такси. По введенным данным-выдает решение задачи с минимальными затратами средств.

--- taxi.py
+++ taxi.py
@@ -1,6 +1,18 @@
 n=int(input("Введите n: "))
-rasst=list(map(int, input("Введите n расстояний через пробел:\n").split()))
-tarifs=list(map(int,input("Введите n тарифов через пробел:\n").split()))
+check=False
+while not check:
+    rasst=list(map(int, input("Введите n расстояний через пробел:\n").split()))
+    if n == len(rasst):
+        check=True
+    else:
+        print("Количество расстояний не совпадает с n")
+check=False
+while not check:
+    tarifs=list(map(int,input("Введите n тарифов через пробел:\n").split()))
+    if n == len(tarifs):
+        check = True
+    else:
+        print("Количество тарифов не совпадает с n")
 
 rasst_sorted=sorted(rasst)
 tarifs_sorted=sorted(tarifs, reverse=True)
@@ -10,4 +22,5 @@ out=[0]*n
 for i in range(n):
     out[rasst.index(rasst_sorted[i])]=tarifs.index(tarifs_sorted[i])+1
 
-print(*out)
+for i in range(n):
+    print(f"{i+1}-й сотрудник должен уехать на {out[i]}-й машине")
